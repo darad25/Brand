eb31ad157bec25ed20c7c7aad12f665a17426ed3
d25 <oladokundara@gmail.com>
2025-01-31 12:08:13 -0800

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter, Abril_Fatface } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-const abrilFatface = Abril_Fatface({ weight: '400', subsets: ['latin'] })
+// const abrilFatface = Abril_Fatface({ weight: '400', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Fashion',
